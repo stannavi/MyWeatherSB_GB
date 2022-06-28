83980f060659471f8bf91fa37350098a87264a00
Stan_Navi <stanlevit@yandex.ru>
2022-06-28 21:15:56 +0300

    Урок 2: 2. познакомились с viewBinding

--- app/src/main/java/com/gb/myweathersb_gb/MainActivity.kt
+++ app/src/main/java/com/gb/myweathersb_gb/MainActivity.kt
@@ -2,10 +2,20 @@ package com.gb.myweathersb_gb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.gb.myweathersb_gb.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btn.text = "Change"
+        //setContentView(R.layout.activity_main)
+        //findViewById<Button>(R.id.btn).text = "Change"
+
+
     }
 }
